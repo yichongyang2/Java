892b6b5b14a42511922ffd19d98e019188982163
yichongyang <33671891+yichongyang@users.noreply.github.com>
2019-03-09 23:22:24 -0500

    Test Changes

--- Compare/src/JavaObjectSorting.java
+++ Compare/src/JavaObjectSorting.java
@@ -10,11 +10,12 @@ public class JavaObjectSorting {
     public static void main(String[] args) {
 
         //sorting custom object array
-        Employee[] empArr = new Employee[4];
+        Employee[] empArr = new Employee[5];
         empArr[0] = new Employee(10, "Mikey", 25, 10000);
         empArr[1] = new Employee(20, "Arun", 29, 20000);
         empArr[2] = new Employee(5, "Lisa", 35, 5000);
         empArr[3] = new Employee(1, "Pankaj", 32, 50000);
+        empArr[4] = new Employee(1, "Sean", 42, 30000);
         
         //sorting employees array using Comparable interface implementation
         Arrays.sort(empArr);
